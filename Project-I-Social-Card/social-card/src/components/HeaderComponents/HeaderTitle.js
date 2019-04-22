c025f5b0f1d7aa10c0b71aba20a419536c111dd3
Triston Armstrong <triston95strong@gmail.com>
2019-04-22 15:01:32 -0500

    Add header component structure

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+    return (
+        <div>
+            <h1>Lambda School <span className="small-grey">@LambdaSchool - 26 jan</span></h1>
+            <HeaderContent />
+        </div>
+        
+    )
+}
+
+export default HeaderTitle;
