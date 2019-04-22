7021d43ee58665ca9f04d63b7a3e966d3273b927
Triston Armstrong <triston95strong@gmail.com>
2019-04-22 14:36:28 -0500

    Setup file structure

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return(
+        <img src="/public/social-card.png" />
+    )
+}
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer = () => {
+ return (
+     <div>
+         
+     </div>
+ )
+}
+
+
+export default HeaderContainer;
