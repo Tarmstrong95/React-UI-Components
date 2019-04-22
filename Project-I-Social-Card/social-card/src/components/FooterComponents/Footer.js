404104a5290a685929a66ecca0f22126797868e9
Triston Armstrong <triston95strong@gmail.com>
2019-04-22 15:07:51 -0500

    Add footer structure

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            
+            <a><i className="far fa-comment"/></a>
+            <a><i className="fas fa-sync"/></a>
+            <a><i className="far fa-heart"/></a>
+            <a><i className="far fa-envelope"/></a>
         </div>
     )
 }
