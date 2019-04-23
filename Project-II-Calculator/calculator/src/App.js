5111179ca32e23dccfec1503f26b49f27a06c8ed
Triston Armstrong <triston95strong@gmail.com>
2019-04-23 15:59:22 -0500

    Complete calculator structure and style

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,81 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
+
+
+const numbers = [
+  {
+    number: "clear",
+    buttonStyle: 'button-large'
+  },
+  {
+    number: "÷",
+    buttonStyle: 'button-small red'
+  },
+  {
+    number: "7",
+    buttonStyle: 'button-small'
+  },
+  {
+    number: "8",
+    buttonStyle: 'button-small'
+  },
+  {
+    number: "9",
+    buttonStyle: 'button-small'
+  },
+  {
+    number: "x",
+    buttonStyle: 'button-small red'
+  },
+  {
+    number: "4",
+    buttonStyle: 'button-small'
+  },
+  {
+    number: "5",
+    buttonStyle: 'button-small'
+  },
+  {
+    number: "6",
+    buttonStyle: 'button-small'
+  },
+  {
+    number: "-",
+    buttonStyle: 'button-small red'
+  },
+  {
+    number: "1",
+    buttonStyle: 'button-small'
+  },
+  {
+    number: "2",
+    buttonStyle: 'button-small'
+  },
+  {
+    number: "3",
+    buttonStyle: 'button-small'
+  },
+  {
+    number: "+",
+    buttonStyle: 'button-small red'
+  },
+  {
+    number: "0",
+    buttonStyle: 'button-large'
+  },
+  {
+    number: "=",
+    buttonStyle: 'button-small red'
+  },
+]
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="app">
+        <CalculatorDisplay />
+        {numbers.map(number => <NumberButton  obj={number} />)}
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+import { prependOnceListener } from 'cluster';
+
+
+const ActionButton = (props) => {
+    return(
+        <button className={props.buttonstyle}>{props.text}</button>
+    )
+}
+
+export default ActionButton;
